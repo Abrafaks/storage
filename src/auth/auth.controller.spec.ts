1063287a60a422e1f3ebea7996a6d69e5e4ce80c
Abrafaks <abrafaks@users.noreply.github.com>
2024-04-21 11:32:26 +0200

    Fix tests by adding required modules and services

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthModule } from './auth.module';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +10,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      imports: [UserModule, AppModule, AuthModule],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
